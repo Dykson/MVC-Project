0dfdc20bacdefecb17a84319451d03a0ad226a06
Dykson <dykson-vd@yandex.ru>
2017-01-23 20:16:23 +0300

    Add files via upload

--- SqliteDBService.cs
+++ SqliteDBService.cs
@@ -4,44 +4,47 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SQLite;
-using MVCProject.Entity;
+using MvcProject.Entity;
 
-namespace MVCProject.Service
+namespace MvcProject.Service
 {
     class SqliteDBService
     {
         private static SQLiteConnection connection;
-        static string response;
+
         public static string getConnectionString()
         {
             return "data source=C:/my_app_db.db";
         }
-
-        public static Hashtable UserTableRequest(string query)
+        public static void CompleteConnection()
         {
-            using (SqliteDBService.connection = new SQLiteConnection(SqliteDBService.getConnectionString()))
-            {
-                SQLiteCommand command = new SQLiteCommand(connection);
-                connection.Open();                           // Open the connection to the database
-                command.CommandText = query; // Select all users from database table
-                SQLiteDataReader reader = command.ExecuteReader();
+            SqliteDBService.connection.Close();
+            SqliteDBService.connection.Dispose();
+        }
 
-                Hashtable response = new Hashtable();
-                
+        public static SQLiteDataReader UserTableRequest(string query)
+        {
+            try
+            {
+                SqliteDBService.connection = new SQLiteConnection(SqliteDBService.getConnectionString());
+                SQLiteCommand command = new SQLiteCommand(connection);
+                connection.Open();
+                command.CommandText = query;
+                SQLiteDataReader reader = command.ExecuteReader();
                 while (reader.Read())
                 {
-                    response["id"] = reader.GetInt32(0);
-                    response["login"] = reader.GetString(1);
-                    response["password"] = reader.GetString(2);
-                    response["firstname"] = reader.GetString(3);
-                    response["lastname"] = reader.GetString(4);
-                    response["position"] = reader.GetString(5);
+                    return reader;
                 }
-
-                SqliteDBService.connection.Close();
-
-                return response;
+                return null;  
             }
+            catch (Exception exception)
+            {
+                SqliteDBService.CompleteConnection();
+                Console.WriteLine(exception.Message);
+
+                return null;
+            }
+       
         }
     }
 }
